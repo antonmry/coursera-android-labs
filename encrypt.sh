2ecfaa508946664294e83e509fa8d4b4aad6baa1
antonmry <ayuste@optaresolutions.com>
2015-10-25 00:21:53 +0200

    Closes #4

--- encrypt.sh
+++ encrypt.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-echo "Please enter the password";
-read -s password;
+while true
+do
+	read -s -p "Password: " password
+	echo
+	read -s -p "Password (again): " password2
+	echo
+	[ "$password" = "$password2" ] && break
+	echo "Please try again"
+done
 
-openssl aes-128-cbc -salt -in AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java -out AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java.aes -k $password 
+# Assignement 2
 
-cp AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java.original AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java
+openssl aes-128-cbc -salt -in TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityOne.java  -out TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityOne.java.aes -k $password
+
+cp TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityOne.java.original TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityOne.java
+
+openssl aes-128-cbc -salt -in TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityTwo.java -out TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityTwo.java.aes -k $password
+
+cp TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityTwo.java.original TheActivityClass/Lab2_ActivityLab/app/src/main/java/course/labs/activitylab/ActivityTwo.java
+
+# Assignement 5
+#openssl aes-128-cbc -salt -in AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java -out AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java.aes -k $password 
+
+#cp AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java.original AsyncTask/Lab5-AsyncTaskLab/app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java
